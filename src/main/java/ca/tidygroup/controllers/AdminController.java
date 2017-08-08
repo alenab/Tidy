457ca8650a67b084eede06d637f83406290e0233
Oleh Kuzych <oleh_kuzych_tp@bmc.com>
2017-08-08 20:03:46 +0300

    Admin controller is accessible only for users with ADMIN role

--- src/main/java/ca/tidygroup/controllers/AdminController.java
+++ src/main/java/ca/tidygroup/controllers/AdminController.java
@@ -7,6 +7,7 @@ import ca.tidygroup.service.PricingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 
+@Secured("ADMIN")
 @RequestMapping("/admin")
 @Controller
 public class AdminController {
