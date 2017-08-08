9cfcdb3be2f034bf39135e18852698d035723b83
Oleh Kuzych <oleh_kuzych_tp@bmc.com>
2017-08-08 22:38:46 +0300

    - Switch cache control back on for spring security; - Set cache period for static resources through application.properties

--- src/main/java/ca/tidygroup/config/WebSecurityConfig.java
+++ src/main/java/ca/tidygroup/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,13 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .headers()
                 .frameOptions().sameOrigin()
-                // Prevent the HTTP response header of "Pragma: no-cache".
-                .cacheControl().disable();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/images/**", "/fonts/**");
+                .cacheControl();
     }
 
     @Autowired
